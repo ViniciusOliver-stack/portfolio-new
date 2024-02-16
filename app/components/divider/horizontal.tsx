c84c7f852a34f7cbbd8c5afc58edacff82b3b820
Vinicius Oliveira <vinicius.oliveira@melius.software>
2024-02-15 21:17:23 -0300

    feat: component Divider

---
+++ app/components/divider/horizontal.tsx
@@ -0,0 +1,13 @@
+import { cn } from '@/app/lib/utils'
+
+interface HorizontalDividerProps {
+  className?: string
+}
+
+export function HorizontalDivider({ className }: HorizontalDividerProps) {
+  return (
+    <div
+      className={cn('w-full my-8 border-b border-b-gray-800', className)}
+    ></div>
+  )
+}
