7e718d3687bf5dfea99c39f609e50a16ff71514a
Vinicius Oliveira <vinicius.oliveira@melius.software>
2024-02-24 21:30:10 -0300

    feat: information page

---
+++ app/utils/info-page.ts
@@ -0,0 +1,83 @@
+export const heroSection = [
+  {
+    description:
+      'Meu interesse pelo desenvolvimento começou há 2 anos, vendo a possibilidade de utilizar a tecnologia para facilitar a vida das pessoas, logo percebi que tinha encontrado a minha paixão e decidi investi em cursos para aprimorar minhas habilidades. Atualmente estou focado em crescer profissionalmente e aprender continuamente criando projetos que me permitam buscar constantemente desafios que me faça explorar e expandir minhas habilidades nessa área. Estou ansioso para me juntar à sua equipe e colaborar nos projetos, estou pronto para fazer acontecer! Vamos trabalhar juntos e criar algo incrível.',
+    links: [
+      {
+        github: {
+          url: 'XXXXXXXXXXXXXXXXXXXXXX',
+          name: 'Github',
+        },
+        linkedin: {
+          url: 'XXXXXXXXXXXXXXXXXXXXXX',
+          name: 'Linkedin',
+        },
+      },
+    ],
+  },
+]
+
+export const techName = [
+  {
+    name: 'JavaScript',
+  },
+  {
+    name: 'Typescript',
+  },
+  {
+    name: 'React.JS',
+  },
+  {
+    name: 'Next.JS',
+  },
+  {
+    name: 'TailwindCSS',
+  },
+  {
+    name: 'Node.JS',
+  },
+]
+
+export const techBadge = [
+  {
+    name: 'JavaScript',
+    icon: 'XXXXXX',
+    startDate: '2022-11-02',
+  },
+  {
+    name: 'React',
+    icon: 'XXXXXX',
+    startDate: '2022-11-02',
+  },
+  {
+    name: 'Typescript',
+    icon: 'XXXXXX',
+    startDate: '2022-11-02',
+  },
+  {
+    name: 'NextJS',
+    icon: 'XXXXXX',
+    startDate: '2022-11-02',
+  },
+]
+
+export const workExperience = [
+  {
+    company: 'Meu IOT',
+    linkCompany: 'https://www.meuiot.com.br/',
+    logoCompany: 'https://www.meuiot.com.br/assets/logoAzul.d1c8dd81.svg',
+    position: 'Desenvolvedor Front-end',
+    startDate: '2022-11-02',
+    endDate: '2023-11-02',
+    description: [
+      'Desenvolvimento de sistema de avaliação para melhorar a qualidade de vida dos moradores',
+      'Implementação de sistema de NPS e votação para medir a satisfação dos usuários',
+      'Criação da funcionalidade de CRUD para o registro de dados do condomínio, permitindo a gestão eficiente das informações essenciais para a administração.',
+      'Desenvolvimento de sistema de permissões para aumentar a segurança e privacidade dos usuários.',
+      'Colaboração com equipe Backend para garantir o bom funcionamento das aplicações.',
+      'Criação e manutenção de interfaces responsivas e intuitivas.',
+      'Acompanhamento das tendências e melhores práticas do desenvolvimento de habilidades técnicas para contribuir de forma efetiva para os projetos da empresa.',
+    ],
+    techsUsed: ['NextJs', 'Typescript', 'TailwindCSS', 'GraphQL'],
+  },
+]
