60a359c72f619570cfafd7249e5cdc3bba6a10ae
Vinicius Oliveira <vinicius.oliveira@melius.software>
2024-02-24 21:31:49 -0300

    feat: component sections

---
+++ app/components/pages/project/project-sections/sections.ts
@@ -0,0 +1,10 @@
+export const sections = [
+  {
+    title: 'Login',
+    image: 'https://illlustrations.co/preview.jpg',
+  },
+  {
+    title: 'Home',
+    image: 'https://illlustrations.co/preview.jpg',
+  },
+]
