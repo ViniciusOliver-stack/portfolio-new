b4f16640575bc29829bb8ec8eaaa5e0bd7362220
Vinicius Oliveira <vinicius.oliveira@melius.software>
2024-02-24 21:35:32 -0300

    feat: create component footer

---
+++ app/components/footer/index.tsx
@@ -0,0 +1,15 @@
+import { IoMdHeart } from 'react-icons/io'
+
+const date = new Date()
+const year = date.getFullYear()
+
+export function Footer() {
+  return (
+    <footer className="h-14 w-full flex items-center justify-center bg-gray-950">
+      <span className="flex items-center gap-1.5 text-xs sm:text-sm font-mono text-gray-400">
+        Made by <IoMdHeart size={13} className="text-emerald-500" /> by{' '}
+        <span className="font-medium">Vinicius Oliveira | {year}.</span>
+      </span>
+    </footer>
+  )
+}
